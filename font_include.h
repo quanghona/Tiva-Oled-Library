0930c25d708fc63195cd7ee57b7cdc61eee786da
quanghona <lyhonquang@gmail.com>
2018-04-22 17:05:37 +0700

    Add bitmap_db and font_include header files

---
+++ font_include.h
@@ -0,0 +1,23 @@
+/*
+ * font_include.h - include all font data
+ * Oled Graphics library
+ *
+ * Date: 14/11/2016
+ * Author: QUANG
+ */
+ 
+#ifndef FONT_INCLUDE_H_
+#define FONT_INCLUDE_H_
+
+//Arial
+#include "arial_8pt.h"
+#include "arial_12pt.h"
+#include "arial_16pt.h"
+#include "arial_20pt.h"
+//Times New Roman
+#include "timesNewRoman_8pt.h"
+#include "timesNewRoman_12pt.h"
+#include "timesNewRoman_16pt.h"
+#include "timesNewRoman_20pt.h"
+
+#endif /* SPI_FONT_FONT_INCLUDE_H_ */
